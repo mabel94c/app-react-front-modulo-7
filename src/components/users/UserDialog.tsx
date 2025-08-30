c7dfd31a74040227c370dd9370b005eb6641ef89
Sara Mabel Siles Calderon <lebam.scalderon@gmail.com>
2025-08-30 19:31:33 -0400

    Update UserDialog.tsx

--- src/components/users/UserDialog.tsx
+++ src/components/users/UserDialog.tsx
@@ -7,18 +7,17 @@ import {
   DialogContent,
   DialogTitle,
   TextField,
-  InputAdornment,
-  IconButton,
 } from '@mui/material';
 import { useState } from 'react';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-
 import type { UserType } from './type';
+import { useActionState } from 'react';
 import type { ActionState } from '../../interfaces';
 import type { UserFormValues } from '../../models';
 import { createInitialState } from '../../helpers';
-import { useActionState } from 'react';
+
+
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export type UserActionState = ActionState<UserFormValues>;
 
@@ -31,20 +30,26 @@ interface Props {
     formData: FormData
   ) => Promise<UserActionState | undefined>;
 }
-
 export const UserDialog = ({ onClose, open, user, handleCreateEdit }: Props) => {
   const initialState = createInitialState<UserFormValues>();
-  const [state, submitAction, isPending] = useActionState(handleCreateEdit, initialState);
 
-  // Estado para visibilidad de contraseña
+  const [state, submitAction, isPending] = useActionState(
+    handleCreateEdit,
+    initialState
+  );
+
+     // Estado para manejar la visibilidad de la contraseña
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleClickShowPassword = () => setShowPassword((prev) => !prev);
-
+  // Función para alternar la visibilidad de la contraseña
+  const handleClickShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+  
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
+    <Dialog open={open} onClose={onClose} maxWidth={'sm'} fullWidth>
       <DialogTitle>{user ? 'Editar Usuario' : 'Nuevo Usuario'}</DialogTitle>
-      <Box component="form" action={submitAction} key={user?.id ?? 'new'}>
+      <Box key={user?.id ?? 'new'} component={'form'} action={submitAction}>
         <DialogContent>
           <TextField
             name="username"
@@ -55,67 +60,56 @@ export const UserDialog = ({ onClose, open, user, handleCreateEdit }: Props) => 
             required
             variant="outlined"
             disabled={isPending}
-            value={state?.formData?.username || user?.username || ''}
-            onChange={(e) =>
-              state.setFormData({ ...state.formData, username: e.target.value })
-            }
+            defaultValue={state?.formData?.username || user?.username || ''}
             error={!!state?.errors?.username}
             helperText={state?.errors?.username}
             sx={{ mb: 2 }}
           />
-
-          {/* Password */}
-          <TextField
-            name="password"
-            margin="normal"
-            required
-            fullWidth
-            label="Password"
-            type={showPassword ? 'text' : 'password'}
-            disabled={isPending}
-            value={state?.formData?.password || user?.password || ''}
-            onChange={(e) =>
-              state.setFormData({ ...state.formData, password: e.target.value })
-            }
-            error={!!state?.errors?.password}
-            helperText={state?.errors?.password}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={handleClickShowPassword} edge="end">
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-
-          <TextField
-            name="confirmPassword"
-            margin="normal"
-            required
-            fullWidth
-            label="Repetir Password"
-            type={showPassword ? 'text' : 'password'}
-            disabled={isPending}
-            value={state?.formData?.confirmPassword || user?.password || ''}
-            onChange={(e) =>
-              state.setFormData({ ...state.formData, confirmPassword: e.target.value })
-            }
-            error={!!state?.errors?.confirmPassword}
-            helperText={state?.errors?.confirmPassword}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={handleClickShowPassword} edge="end">
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
+          {/* Solo si es nuevo */}
+ 
+            <TextField
+              name="password"
+              margin="normal"
+              required
+              fullWidth
+              label="Password"
+               type={showPassword ? 'text' : 'password'} // Alterna el tipo según el estado
+              disabled={isPending}
+              defaultValue={state?.formData?.password || user?.password || ''}
+              error={!!state?.errors?.password}
+              helperText={state?.errors?.password}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleClickShowPassword} edge="end">
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              name="confirmPassword"
+              margin="normal"
+              required
+              fullWidth
+              label="Repetir password"
+              type={showPassword ? 'text' : 'password'} // Alterna el tipo según el estado
+              disabled={isPending}
+              defaultValue={state?.formData?.confirmPassword || user?.password || ''}
+              error={!!state?.errors?.confirmPassword}
+              helperText={state?.errors?.confirmPassword}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleClickShowPassword} edge="end">
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            /> 
         </DialogContent>
-
         <DialogActions sx={{ p: 2 }}>
           <Button onClick={onClose} color="inherit" disabled={isPending}>
             Cancelar
@@ -125,7 +119,7 @@ export const UserDialog = ({ onClose, open, user, handleCreateEdit }: Props) => 
             variant="contained"
             color="primary"
             disabled={isPending}
-            startIcon={isPending ? <CircularProgress size={20} /> : null}
+            startIcon={isPending ? <CircularProgress /> : null}
           >
             {user ? 'Actualizar' : 'Crear'}
           </Button>
